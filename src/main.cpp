2dbb9367ad7fd395198d3376f3e60a998bc9cd5f
mheshamg <mheshamg@gmail.com>
2021-06-02 00:19:03 +0200

    Add code for the Boost Asio crash course

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,56 @@
 #include <boost/system/error_code.hpp>
 
 #include <iostream>
+#include <thread>
+#include <iomanip>
+
+using tcp = boost::asio::ip::tcp;
+
+void log(boost::system::error_code ec)
+{
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] "
+              << (ec ? "Error: " : "OK")
+              << (ec ? ec.message() : "")
+              << std::endl;
+}
+
+void onConnect(boost::system::error_code ec)
+{
+    log(ec);
+}
 
 int main()
 {
+    std::cerr << "[" << std::setw(14) << std::this_thread::get_id() << "] main"
+              << std::endl;
+    boost::asio::io_context ioc{};
+
+    tcp::socket socket{boost::asio::make_strand(ioc)};
+
+    size_t nThreads {4};
+
     // A 'falsey' error_code means "no error".
     boost::system::error_code ec {};
+    tcp::resolver resolver {ioc};
+    auto resolverIt {resolver.resolve("google.com","80",ec)};
     if (ec) {
-        std::cerr << "Error: " << ec.message() << std::endl;
+        log(ec);
         return -1;
-    } else {
-        std::cout << "OK" << std::endl;
-        return 0;
     }
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        socket.async_connect(*resolverIt, onConnect);
+    }
+
+    // We must call io_context::run for asynchronous callbacks to run.
+    std::vector<std::thread> threads {};
+    threads.reserve(nThreads);
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        threads.emplace_back([&ioc]() {
+            ioc.run();
+        });
+    }
+    for (size_t idx {0}; idx < nThreads; ++idx) {
+        threads[idx].join();
+    }
+    return 0;
 }
