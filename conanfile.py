479e39869d2b2ce863cac16171c8e7f4b63d5f3e
mheshamg <mheshamg@gmail.com>
2021-07-16 20:26:27 +0200

    Add function to download file from HTTPS URL

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,12 @@ class ConanPackage(ConanFile):
 
     requires = [
         ('boost/1.74.0'),
-        ('openssl/1.1.1h')
+        ('openssl/1.1.1h'),
+        ('libcurl/7.77.0')
     ]
 
     default_options = (
         'boost:shared=False',
-        'openssl:shared=False'
+        'openssl:shared=False',
+        'libcurl:shared=False'
     )
